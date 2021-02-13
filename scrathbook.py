fea17c75561681ad8876262fecb41156bd24945a
Arvid A <arvid.anderson@outlook.com>
2021-02-13 22:39:55 +0100

    Delete scrathbook.py
    Had for testing

--- scrathbook.py
+++
@@ -1,9 +0,0 @@
-            amount_int = int(amount)
-            if amount_int < balance:
-                sg.popup(f"You are trying to withdraw more money then you currently have on your bankaccount, You have a total of ${balance}")
-                window.close()
-                withdraw_form()
-            else:
-                utilities.withdraw_balance(file_name, amount_int)
-                window.close()
-                gui_profile.profile_page()
