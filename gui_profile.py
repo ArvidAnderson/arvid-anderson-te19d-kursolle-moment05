3df6fc3832424be34dd7f7ff0da1a2df42742f30
Arvid A <arvid.anderson@outlook.com>
2021-02-13 19:13:00 +0100

    Init
    Added the Welcome {user_name} to the gui_profile.py

--- gui_profile.py
+++ gui_profile.py
@@ -6,12 +6,13 @@ import utilities
 
 def profile_page():
 
-    print(utilities.get_current_user_str())
+    file_name = (utilities.get_current_user_str())
+    fname = utilities.get_fname(file_name)
 
     sg.theme('DarkTeal4')
 
-    layout = [[sg.Text('SCAM-BANK™ LLC', size=(30, 1), font='Courier 20')],
-            [sg.Text("Welcome")],       
+    layout = [[sg.Text('SCAM-BANK™ LLC', size=(30, 1), font='Helvetica 20')],
+            [sg.Text(f"Welcome {fname}, thanks for choosing us!", font='Helvetica 12')],       
             [sg.Button('Deposit'), sg.Button('Withdraw'), sg.Button('Transactions'), sg.Button('Terminate Account'), sg.Exit()]]      
 
     window = sg.Window('SCAM-BANK™', layout)      
